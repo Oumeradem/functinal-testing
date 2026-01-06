f8219a78f2393cb2972be81f9589f6b5ba8b273d
oumer <oumer.adamye@gmail.com>
2026-01-06 15:19:31 -0800

    Click Next Button Step2 Completed

--- steps/CommonSteps.ts
+++ steps/CommonSteps.ts
@@ -1,4 +1,5 @@
 import { Given, Then, When } from "@cucumber/cucumber";
+import { faker } from "@faker-js/faker";
 import { expect } from "@playwright/test";
 import {
   startApplicationPage,
@@ -11,4 +12,21 @@ import {
 
 Given('user is on the enrollment page', async function () {
     await startApplicationPage.login();
+    
 });
+
+Given(
+  "user already completed the start application page",
+  async function () {
+    await startApplicationPage.firstNameInputBox.fill(faker.person.firstName());
+    await startApplicationPage.lastNameInputBox.fill(faker.person.lastName());
+    await startApplicationPage.emailInputBox.fill(faker.internet.email());
+    await startApplicationPage.phoneNumberInputBox.fill(
+      faker.string.numeric(10));
+
+     // await page.waitForTimeout(5000);
+      
+      await startApplicationPage.nextButton.click();
+
+  }
+);
--- steps/payment-plans/clickNextButtonStep2.ts
+++ steps/payment-plans/clickNextButtonStep2.ts
@@ -1,23 +1,92 @@
-import { Given, Then, When } from "@cucumber/cucumber";
-import { expect} from "@playwright/test";
-import { paymentPlanPage, page } from "../../globalPagesSetup";
+import { Then, When } from "@cucumber/cucumber";
+import { expect } from "@playwright/test";
+import {
+  paymentPlanPage,
+  page,
+  startApplicationPage,
+} from "../../globalPagesSetup";
 import { productInfo } from "../../utilities/qa-data-reader";
 
-
-Given('user has completed step one with valid information', async function () {
+Then("the next button is disabled by default", async function () {
+  await expect(paymentPlanPage.inactiveNextButton).toBeDisabled();
 });
 
-Given('user is on step two of the enrollment process', async function () {
+When("user selects upfront payment plan", async function () {
+  await paymentPlanPage.upfrontPaymentOption.click();
 });
 
-Then('the next button is disabled by default', async function () {
+Then("the next button is enabled", async function () {
+  await expect(paymentPlanPage.inactiveNextButton).toBeEnabled();
+
+  await page.waitForTimeout(5000);
 });
 
-When('user clicks upfront payment option', async function () {
+Then("the step1 stepper circle is green", async function () {
+  await expect(startApplicationPage.startApplicationStepCircle).toHaveCSS(
+    "background-color",
+    "rgb(172, 245, 138)"
+  );
 });
 
-Then('the next button will be enabled', async function () {
+Then("the step2 stepper circle is blue", async function () {
+  await expect(startApplicationPage.paymentPlanStepCircle).toHaveCSS(
+    "background-color",
+    "rgb(1, 201, 255)"
+  );
 });
 
-When('user clicks installments  payment option', async function () {
+When("user clicks the next button on payment pale page", async function () {
+  await paymentPlanPage.activeNextButton.click();
+});
+
+Then("the step3 stepper circle is blue", async function () {
+  await expect(startApplicationPage.reviewStepCircle).toHaveCSS(
+    "background-color",
+    "rgb(1, 201, 255)"
+  );
+});
+
+Then("the step2 stepper circle is green", async function () {
+  await expect(startApplicationPage.startApplicationStepCircle).toHaveCSS(
+    "background-color",
+    "rgb(172, 245, 138)"
+  );
+});
+
+Then("the upfront payment summary is displayed", async function () {
+  await expect(paymentPlanPage.basePriceAmountUnderUpfront).toBeVisible();
+  await expect(paymentPlanPage.upfrontDiscountAmountUnderUpfront).toBeVisible();
+  await expect(paymentPlanPage.subtotalAmountUnderUpfront).toBeVisible();
+});
+
+When("user selects instalment payment  paln", async function () {
+  await paymentPlanPage.installmentsPaymentFrame.click();
+});
+
+Then("the instalment payment summary is displayed", async function () {
+  await expect(paymentPlanPage.basePriceAmountUnderInstallments).toBeVisible();
+  await expect(
+    paymentPlanPage.installmentsNumberUnderInstallments
+  ).toBeVisible();
+  await expect(
+    paymentPlanPage.pricePerInstallmentsAmountUnderInstallments
+  ).toBeVisible();
+  await expect(
+    paymentPlanPage.firstMonthPaymentAmountUnderInstallments
+  ).toBeVisible();
+});
+
+Then("the back button is displayed", async function () {
+  await expect(paymentPlanPage.backButton).toBeVisible();
+});
+
+When("user clicks the back button on payment plan page", async function () {
+  await paymentPlanPage.backButton.click();
+});
+
+When("the step1 stepper circle is blue", async function () {
+  await expect(startApplicationPage.startApplicationStepCircle).toHaveCSS(
+    "background-color",
+    "rgb(1, 201, 255)"
+  );
 });
