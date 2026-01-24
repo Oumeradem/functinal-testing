7beacc835ad4af2ee2165e776fc0fae026f03251
oumer <oumer.adamye@gmail.com>
2026-01-24 08:06:47 -0800

    initail commit

--- steps/CommonSteps.ts
+++ steps/CommonSteps.ts
@@ -1,10 +1,7 @@
 import { Given, Then, When } from "@cucumber/cucumber";
-import { faker } from "@faker-js/faker";
 import { expect } from "@playwright/test";
 import { startApplicationPage} from "../globalPagesSetup";
 
 Given("user is on the enrollment page", async function () {
   await startApplicationPage.login();
 });
-
-
